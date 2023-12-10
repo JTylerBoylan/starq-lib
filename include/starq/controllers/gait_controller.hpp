b87419f1f46814ae7d792f3b85d02aa40bf9b360
Jonathan Boylan <jtylerboylan@outlook.com>
2023-12-10 00:21:35 -0500

    RobotController

--- include/starq/controllers/gait_controller.hpp
+++ include/starq/controllers/gait_controller.hpp
@@ -1,41 +1,17 @@
 #ifndef STARQ_CONTROLLERS__GAIT_CONTROLLER_HPP_
 #define STARQ_CONTROLLERS__GAIT_CONTROLLER_HPP_
 
-#include "starq/publishers/leg_command_publisher.hpp"
-#include "starq/slam/localization.hpp"
-#include "starq/slam/terrain_map.hpp"
+#include <memory>
+#include "eigen3/Eigen/Dense"
 
 namespace starq::controllers
 {
+    using namespace Eigen;
 
     class GaitController
     {
     public:
         using Ptr = std::shared_ptr<GaitController>;
-
-        /// @brief Create a gait controller.
-        /// @param leg_command_publisher Leg command publisher.
-        GaitController(const starq::publishers::LegCommandPublisher::Ptr leg_command_publisher,
-                       const starq::slam::Localization::Ptr localization,
-                       const starq::slam::TerrainMap::Ptr terrain_map)
-            : leg_command_publisher_(leg_command_publisher),
-              localization_(localization),
-              terrain_map_(terrain_map) {}
-
-        /// @brief Destroy the gait controller.
-        ~GaitController() {}
-
-        /// @brief Get the leg command publisher.
-        /// @return Leg command publisher.
-        starq::publishers::LegCommandPublisher::Ptr getLegCommandPublisher() { return leg_command_publisher_; }
-
-        /// @brief Get the localization.
-        /// @return Localization.
-        starq::slam::Localization::Ptr getLocalization() { return localization_; }
-
-        /// @brief Get the terrain map.
-        /// @return Terrain map.
-        starq::slam::TerrainMap::Ptr getTerrainMap() { return terrain_map_; }
 
         /// @brief Get the name of the gait.
         /// @return Name of the gait.
@@ -47,9 +23,6 @@ namespace starq::controllers
         virtual void moveTo(const Vector3f &position, const Vector3f &orientation) = 0;
 
     private:
-        starq::publishers::LegCommandPublisher::Ptr leg_command_publisher_;
-        starq::slam::Localization::Ptr localization_;
-        starq::slam::TerrainMap::Ptr terrain_map_;
     };
 
 }
