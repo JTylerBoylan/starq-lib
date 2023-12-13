b527f7f056c082f9e5f4ea39295f6b18d0e17d73
Jonathan Boylan <jtylerboylan@outlook.com>
2023-12-12 19:50:33 -0500

    Walking work

--- include/starq/walking/walking_gait_planner.hpp
+++ include/starq/walking/walking_gait_planner.hpp
@@ -8,10 +8,23 @@ namespace starq::walking
 
     struct CenterOfMassTrajectory
     {
+        size_t size;
         std::vector<Eigen::Vector3f> positions;
         std::vector<Eigen::Vector3f> orientations;
         std::vector<Eigen::Vector3f> velocities;
         std::vector<Eigen::Vector3f> angular_velocities;
+    };
+
+    struct FootfallState
+    {
+        std::vector<bool> in_stance;
+        std::vector<Eigen::Vector3f> positions;
+    };
+
+    struct FootfallPattern
+    {
+        size_t size;
+        std::vector<FootfallState> states;
     };
 
     class WalkingGaitPlanner
@@ -25,17 +38,26 @@ namespace starq::walking
         /// @brief Destroy the walking gait planner.
         ~WalkingGaitPlanner();
 
+        /// @brief Set the desired state.
+        /// @param position Desired position.
+        /// @param orientation Desired orientation.
+        void setDesiredState(const Eigen::Vector3f &position, const Eigen::Vector3f &orientation);
+
         /// @brief Get the center of mass trajectory.
-        /// @param goal_position Goal position.
-        /// @param goal_orientation Goal orientation.
         /// @param trajectory Center of mass trajectory.
         /// @return True if the trajectory was generated successfully, false otherwise.
-        bool getCenterOfMassTrajectoryToGoal(const Eigen::Vector3f &goal_position,
-                                             const Eigen::Vector3f &goal_orientation,
-                                             CenterOfMassTrajectory &trajectory);
+        bool getCenterOfMassTrajectory(CenterOfMassTrajectory &trajectory);
+
+        /// @brief Get the footfall pattern.
+        /// @param pattern Footfall pattern.
+        /// @return True if the pattern was generated successfully, false otherwise.
+        bool getFootfallPattern(FootfallPattern &pattern);
 
     private:
         starq::slam::Localization::Ptr localization_;
+
+        Eigen::Vector3f desired_position_;
+        Eigen::Vector3f desired_orientation_;
     };
 
 }
