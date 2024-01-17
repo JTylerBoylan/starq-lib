75c9751c70b7eb4d9bdaf587630079a603fc28f4
Jonathan Boylan <jtylerboylan@outlook.com>
2024-01-17 15:08:49 -0500

    matlab

--- matlab/FiveBar2D_dynamics.m
+++ matlab/FiveBar2D_dynamics.m
@@ -7,11 +7,11 @@ close all
 syms L1_M L2_M
 syms thetaA thetaB
 
-alpha_f = 0.5 * (pi + thetaA + thetaB);
+alpha_f = 0.5 * (pi - thetaA - thetaB);
 gamma_f = asin(L1_M*sin(alpha_f)/L2_M);
 phi_f = pi - alpha_f - gamma_f;
 
-theta_f = thetaA - alpha_f;
+theta_f = thetaA + alpha_f;
 R_f = L2_M*sin(phi_f)/sin(alpha_f);
 
 X_f = R_f*cos(theta_f);
@@ -25,6 +25,10 @@ forward_jacobian = simplify(J_f)
 
 getForwardJacobian = matlabFunction(forward_jacobian);
 
+Jf_11 = forward_jacobian(1,1)
+Jf_12 = forward_jacobian(1,2)
+Jf_21 = forward_jacobian(2,1)
+Jf_22 = forward_jacobian(2,2)
 
 %% Test
 
